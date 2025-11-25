466013862ff67584d73b74cd0cf5b993f07f70ba
Kostenko-Yaroslav <yaroslavkostenko34@gmail.com>
2025-11-25 18:16:11 +0200

    Add alert verification

--- schedule/tasks.py
+++ schedule/tasks.py
@@ -5,6 +5,7 @@ from schedule.models import Lesson
 import time
 from bot.bot_instance import bot
 
+import urllib.request, json
 
 @periodic_task(crontab(minute='*/1'))
 def open_window():
@@ -18,6 +19,12 @@ def open_window():
     lesson_start = lessons.filter(bell__time_start__hour=now.hour, bell__time_start__minute=now.minute)
     lesson_end = lessons.filter(bell__time_end__hour=now.hour, bell__time_end__minute=now.minute)
 
+    try:
+        with urllib.request.urlopen('https://ubilling.net.ua/aerialalerts/') as url:
+            data = json.loads(url.read().decode())
+    except:
+        data = None
+
     def lesson_message(lesson, is_start):
         try:
             if hasattr(lesson.user, 'profile' and lesson.user.profile.chat_id):
@@ -25,10 +32,19 @@ def open_window():
 
             chat_id = lesson.user.profile.chat_id
 
-            if is_start:
-                text = f'Пара: {lesson.subject.name} НАЧАЛАСЬ ! Link: {lesson.subject.link};'
+            if lesson.user.profile.city in data['states']:
+                alert_status = data['states'][lesson.user.profile.city]['alertnow']
             else:
-                text = f'Пара: {lesson.subject.name} ЗАКОНЧИЛАСЬ ! Link: {lesson.subject.link};'
+                alert_status = False
+
+
+            if alert_status:
+                text = f'Тревога!'
+            else:
+                if is_start:
+                    text = f'Пара: {lesson.subject.name} НАЧАЛАСЬ ! Link: {lesson.subject.link};'
+                else:
+                    text = f'Пара: {lesson.subject.name} ЗАКОНЧИЛАСЬ ! Link: {lesson.subject.link};'
             profile = lesson.user.profile.repeats
 
             for _ in range(profile):
